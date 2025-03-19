015df71182819eb31f387fe3859307088f0059de
Ariane Bordeleau <bordeleauariane8@gmail.com>
2025-03-19 13:33:36 -0400

    credentials form, after create a credential, clean up the state and the form

--- hooks/use-credentials-store.ts
+++ hooks/use-credentials-store.ts
@@ -1,93 +1,103 @@
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
-import { credentialSchema, Asset } from "@/schemas/credential";
-import apiClient from "@/lib/apiService"; // API client
+import { WritableDraft } from "immer";
 import { Credential } from "@/openapi-types";
 
 type Mode = "create" | "import" | "view";
 
 interface State {
-	selectedCredential: Credential | null;
-	mode: Mode;
-	isCreating: boolean;
-	isDeleting: boolean;
-	credentials: Credential[];
+  selectedCredential: Credential | null;
+  mode: Mode;
+  isCreating: boolean;
+  isDeleting: boolean;
+  credentials: Credential[];
 }
 
 interface Actions {
-	setSelectedCredential: (credential: Credential | null) => void;
-	startCreating: () => Promise<string>;
-	startImporting: () => void;
-	viewCredential: (credential: Credential) => void;
-	cancel: () => void;
-	reset: () => void;
-	deleteCredential: (credentialId: string) => void;
+  setSelectedCredential: (credential: Credential | null) => void;
+  startCreating: () => Promise<string>;
+  startImporting: () => void;
+  viewCredential: (credential: Credential) => void;
+  cancel: () => void;
+  reset: () => void;
+  deleteCredential: (credentialId: string) => void;
 }
 
 export const useCredentials = create<State & Actions>()(
-	immer((set, get) => ({
-		selectedCredential: null,
-		mode: "create",
-		credentials: [],
-		isCreating: false,
-		isDeleting: false,
+  immer((set, get) => ({
+    selectedCredential: null,
+    mode: "create",
+    credentials: [],
+    isCreating: false,
+    isDeleting: false,
 
-		deleteCredential: async () => {
-			set((state) => {
-				state.selectedCredential = null;
-				state.mode = "create";
-			});
-		},
+    deleteCredential: async () => {
+      set((state) => {
+        state.selectedCredential = null;
+        state.mode = "create";
+      });
+    },
 
-		setSelectedCredential: (credential) =>
-			set((state) => {
-				state.selectedCredential = credential;
-			}),
+    setSelectedCredential: (credential) =>
+      set((state) => {
+        state.selectedCredential = credential;
+      }),
 
-		startCreating: async () => {
-			set((state) => {
-				state.selectedCredential = null;
-				state.isCreating = true;
-				state.mode = "create";
-			});
+    startCreating: async () => {
+      const newCredential = {
+        id: Date.now().toString(),
+        name: "",
+        version: "",
+        type: "ANONCRED",
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+        credentialSchema: {
+          id: "",
+          name: "",
+          version: "",
+          createdAt: new Date().toISOString(),
+          updatedAt: new Date().toISOString(),
+        },
+      } as WritableDraft<Credential>;
 
-			const newId = Date.now().toString();
-			set((state) => {
-				state.selectedCredential = { id: newId } as Credential;
-			});
+      set((state) => {
+        state.selectedCredential = newCredential;
+        state.isCreating = true;
+        state.mode = "create";
+      });
 
-			return newId;
-		},
+      return newCredential.id;
+    },
 
-		startImporting: () =>
-			set((state) => {
-				state.mode = "import";
-				state.isCreating = false;
-				state.selectedCredential = null;
-			}),
+    startImporting: () =>
+      set((state) => {
+        state.mode = "import";
+        state.isCreating = false;
+        state.selectedCredential = null;
+      }),
 
-		viewCredential: (credential) => {
-			set((state) => {
-				state.selectedCredential = credential; // Set the selected credential
-				state.mode = "view"; // Set the mode to view
-				state.isCreating = false; // Set isCreating to false since you're not creating
-			});
-		},
+    viewCredential: (credential) => {
+      set((state) => {
+        state.selectedCredential = credential;
+        state.mode = "view";
+        state.isCreating = false;
+      });
+    },
 
-		// After viewing, ensure a full reset for creating:
-		cancel: () => {
-			set((state) => {
-				state.selectedCredential = null; // Reset the selectedCredential
-				state.isCreating = false; // Stop creating mode
-				state.mode = "create"; // Switch back to create mode
-			});
-		},
+    cancel: () => {
+      set((state) => {
+        state.selectedCredential = null;
+        state.isCreating = false;
+        state.mode = "create";
+      });
+    },
 
-		reset: () =>
-			set((state) => {
-				state.selectedCredential = null;
-				state.mode = "create";
-				state.isCreating = false;
-			}),
-	}))
+    reset: () =>
+      set((state) => {
+        state.selectedCredential = null;
+        state.mode = "create";
+        state.isCreating = false;
+      }),
+  }))
 );
+
